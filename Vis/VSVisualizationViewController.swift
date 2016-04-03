0c35d1f9fef4990a0355aa0f7ed87722cb91e664
Jim Boulter <jboulter11@gmail.com>
2016-04-02 20:18:09 -0400

    Visualization View Controller added

---
+++ Vis/VSVisualizationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VSVisualizationViewController.swift
+//  Vis
+//
+//  Created by Jim Boulter on 4/2/16.
+//  Copyright © 2016 Squad. All rights reserved.
+//
+
+import Cocoa
+
+class VSVisualizationViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
