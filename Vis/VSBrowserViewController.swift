cb700cfa59e5cf2280873cec3699f0974e1e2901
Jim Boulter <jboulter11@gmail.com>
2016-04-02 20:16:16 -0400

    on our way

--- Vis/VSBrowserViewController.swift
+++ Vis/VSBrowserViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import Cocoa
+import FileKit
 
 class VSBrowserViewController: NSViewController, NSBrowserDelegate {
 
     @IBOutlet weak var browser: NSBrowser!
+    var subpath :Path = Path.Current
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    func browser(sender: NSBrowser, numberOfRowsInColumn column: Int) -> Int {
+        if column == 0 {
+            return subpath.children(recursive:false).count
+        } else {
+            return Path.Current.children(recursive:false).count
+        }
+    }
+    
+    func browser(sender: NSBrowser, willDisplayCell cell: AnyObject, atRow row: Int, column: Int) {
+        var path: Path
+        if column == 0 {
+            path = Path.Current
+        } else {
+            path = subpath
+        }
+        
+        cell.setTitle(path.children()[row].fileName)
+//        cell.leaf(!path.children()[row].isDirectory)
+    }
     
 }
