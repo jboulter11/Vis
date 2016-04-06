488089b518354c0e4a46f2607c9d157d906c7ef3
Richie <richiejoeyoung@gmail.com>
2016-04-06 17:57:31 -0400

    adds path change listener to visualzation view controller

--- Vis/VSVisualizationViewController.swift
+++ Vis/VSVisualizationViewController.swift
@@ -20,9 +20,13 @@ class VSVisualizationViewController: NSViewController, TreeMapViewDataSource, Tr
         // Do view setup here.
 //        treeMapView.delegate
         treeMapView.reloadData()
-        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(didReceiveNotification), name: "SelectedPathDidChange", object: nil)
     }
     
+    func didReceiveNotification() {
+        let path = VSExec.exec.selectedPath.components.map{$0.rawValue}
+        treeMapView.reloadAndPerformZoomIntoItem(path as [AnyObject])
+    }
     
     // TreeMapViewDataSource Implementation
     
@@ -30,7 +34,7 @@ class VSVisualizationViewController: NSViewController, TreeMapViewDataSource, Tr
         if let i = item {
             return Path(String(i))
         } else {
-            return Path("/Users/richie/Dev/rcos-yacs/yacs")
+            return VSExec.exec.selectedPath
         }
     }
     
