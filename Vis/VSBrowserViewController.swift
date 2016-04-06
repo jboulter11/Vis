0bbc5a05017031a96a1e8b552fd1c09d1855aacc
Jim Boulter <jboulter11@gmail.com>
2016-04-06 16:16:31 -0400

    Fixed browser

--- Vis/VSBrowserViewController.swift
+++ Vis/VSBrowserViewController.swift
@@ -19,39 +19,40 @@ class VSBrowserViewController: NSViewController {
         // Do view setup here.
         
         Path.Current = Path()
-//        browser.action = #selector(self.didSelectSomething)
+        browser.action = #selector(self.didSelectSomething)
+    }
+    
+    func parentNodeForColumn(column: Int) -> Path {
+        var result = Path.Root
+        for i in 0..<column {
+            result = result.children(recursive: false)[browser.selectedRowInColumn(i)]
+        }
+        return result
     }
     
     func browser(sender: NSBrowser, numberOfRowsInColumn column: Int) -> Int {
-        if column == 1 {
-            return subpath.children(recursive:false).count
-        } else {
-            return Path.Current.children(recursive:false).count
-        }
+        let parent = parentNodeForColumn(column)
+        return parent.children(recursive: false).count
     }
     
     func browser(sender: NSBrowser, willDisplayCell cell: AnyObject, atRow row: Int, column: Int) {
-        var path: Path
-        if column == 0 {
-            path = Path.Current
-        } else {
-            path = subpath
-        }
         let theCell:NSBrowserCell = cell as! NSBrowserCell
-        if row < path.children().count {
-            theCell.title = path.children()[row].fileName
-            theCell.leaf = !path.children()[row].isDirectory
+        
+        let path:Path = parentNodeForColumn(column)
+        
+        if row < path.children(recursive: false).count {
+            theCell.title = path.children(recursive: false)[row].fileName
+            theCell.leaf = !path.children(recursive: false)[row].isDirectory
         } else {
             theCell.title = ""
             theCell.leaf = false
         }
-        
-        let _:String = String(path)
     }
     
     func didSelectSomething() {
-        let row = browser.selectedRowInColumn(0)
-        subpath = Path.Current.children()[row]
-        browser.reloadColumn(1)
+        let row = browser.selectedRowInColumn(browser.selectedColumn)
+        let parent = parentNodeForColumn(browser.selectedColumn)
+        let path = parent.children(recursive: false)[row]
+        NSNotificationCenter.defaultCenter().postNotificationName("SelectedPathDidChange", object: path.rawValue)
     }
 }
