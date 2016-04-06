885339a2cf966b158fe4849d6f5a0c2cc12ac130
Richie <richiejoeyoung@gmail.com>
2016-04-06 15:35:43 -0400

    adds supporting files for TreeMapView and implements TreeMapView DataSource and Delegate interfaces

--- Vis/VSVisualizationViewController.swift
+++ Vis/VSVisualizationViewController.swift
@@ -7,12 +7,70 @@
 //
 
 import Cocoa
+import FileKit
 
-class VSVisualizationViewController: NSViewController {
+class VSVisualizationViewController: NSViewController, TreeMapViewDataSource, TreeMapViewDelegate {
 
+    @IBOutlet weak var treeMapView: TreeMapView!
+    
+    let fileTypeColors: FileTypeColors = FileTypeColors()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+//        treeMapView.delegate
+        treeMapView.reloadData()
+        
     }
     
+    
+    // TreeMapViewDataSource Implementation
+    
+    func asPath(item: AnyObject?) -> Path {
+        if let i = item {
+            return Path(String(i))
+        } else {
+            return Path("/Users/richie/Dev/rcos-yacs/yacs")
+        }
+    }
+    
+    func treeMapView(view: TreeMapView!, child index: UInt32, ofItem item: AnyObject?) -> AnyObject! {
+        let path: Path = asPath(item)
+        print(path)
+        return path.children(recursive: false)[Int(index)].rawValue as AnyObject
+    }
+    
+    func treeMapView(view: TreeMapView!, isNode item: AnyObject?) -> Bool {
+//        let path = toPath(item)
+        return true
+    }
+    
+    func treeMapView(view: TreeMapView!, numberOfChildrenOfItem item: AnyObject?) -> UInt32 {
+        let path: Path = asPath(item)
+        let children = path.children()
+        return UInt32(path.children().count)
+    }
+    
+    func treeMapView(view: TreeMapView!, weightByItem item: AnyObject?) -> UInt64 {
+        return asPath(item).fileSize!
+    }
+    
+    // TreeMapViewDelegate Implementation
+    
+    func treeMapView(view: TreeMapView!, getToolTipByItem item: AnyObject?) -> String! {
+        return asPath(item).description
+    }
+    
+    func treeMapView(view: TreeMapView!, willDisplayItem item: AnyObject?, withRenderer renderer: TMVItem!) {
+        let path = asPath(item)
+        renderer.setCushionColor(fileTypeColors.colorForKind(path.fileType?.rawValue))
+    }
+    
+    func treeMapView(view: TreeMapView!, willShowMenuForEvent event: NSEvent!) {
+        
+    }
+    
+    func treeMapView(view: TreeMapView!, shouldSelectItem item: AnyObject?) -> Bool {
+        return false;
+    }
 }
